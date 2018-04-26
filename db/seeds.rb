7a3763fdabebbd7ddc63f1b0e2a6cea889d52179
William Murphy <wmurphy@ventanex.com>
2018-04-25 22:06:23 -0500

    Add seeded users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by!(email: "clmurphy789@gmail.com") do |user|
+  user.password = "cM!@#456"
+  user.is_admin = true
+  user.first_name = "Clinton"
+  user.last_name = "Murphy"
+end
+
+User.find_or_create_by!(email: "chandlercorey15@gmail.com") do |user|
+  user.password = "cC!@#456"
+  user.is_admin = true
+  user.first_name = "Corey"
+  user.last_name = "Chandler"
+end
+
+User.find_or_create_by!(email: "willardtwashington@gmail.com") do |user|
+  user.password = "wM!@#456"
+  user.is_admin = true
+  user.first_name = "William"
+  user.last_name = "Murphy"
+end
